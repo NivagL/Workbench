260be6c980bfa761a75eab5f536cdb3b2676ea06
Mick Buckley <mick.buckley@northpower.com>
2018-09-14 13:35:21 +1200

    Basic fake-data mat-table

--- src/app/component/workorder/workorder.component.ts
+++ src/app/component/workorder/workorder.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface WorkorderJSON {
+  id: number,
+  title: string,
+};
+
 @Component({
   selector: 'app-workorder',
   templateUrl: './workorder.component.html',
@@ -7,9 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkorderComponent implements OnInit {
 
+  public work_orders: Array<WorkorderJSON>;
+  public columnsToDisplay = ['workorderId', 'workorderTitle'];
+
+
   constructor() { }
 
   ngOnInit() {
+    this.work_orders = [
+      { id: 1, title: 'First', },
+      { id: 2, title: 'Second', },
+    ];
   }
 
 }
