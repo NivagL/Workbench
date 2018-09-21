043b411a359ffffe868f8b68144188f2fef35d28
Mick Buckley <mick.buckley@northpower.com>
2018-09-21 15:24:55 +1200

    more theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ToolbarService } from './service/toolbar.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   Title: Observable<string>;
